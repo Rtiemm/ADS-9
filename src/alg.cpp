bde99711990e88f4e85b884dbbfad01f1d3f6b04
Rtiemm <99262095+Rtiemm@users.noreply.github.com>
2022-05-30 01:09:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,32 @@
-// Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <locale>
+#include <string>
 
-BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+#include "bst.h"
+
+BST<std::string> makeTree(const char* name) {
+  BST<std::string> tree;
+  std::ifstream file(name);
+  if (!file) {
+    std::cout << "Something wrong"
+              << "\n";
+    throw std::string("Can not open file!");
+  }
+  std::string word = "";
+  while (!file.eof()) {
+    char IdontWantToDoIt = file.get();
+    int ch = IdontWantToDoIt;
+    if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) {
+      word += tolower(ch);
+    } else {
+      if (word != "") {
+        tree.Add(word);
+        word = "";
+      }
+    }
+  }
+  file.close();
+  return tree;
 }
